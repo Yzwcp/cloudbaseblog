fe60dc34f04cc273c30acfab4eeca6b8c6717ff0
Umep <1494993218@qq.com>
2022-03-25 15:11:10 +0800

    cdn

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,23 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Antd,{message} from 'ant-design-vue';
+import Antd, { message } from 'ant-design-vue';
 import './index.less'
 import "vditor/dist/index.css"
 import 'ant-design-vue/dist/antd.less';
-import {API,} from '@/util/api.js'
+import { API, } from '@/util/api.js'
 import store from './store'
-
+// import '@/util/md5.js'
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('UMEP_BLOG')
-  console.log(token)
-  if(to.meta.admin && !to.path.includes('login')){
-    if(token) return  next()
-    message.info('请登录')
-    next({path:'/login'})
-  }else{
-    next()
-  }
+    const token = localStorage.getItem('UMEP_BLOG')
+    console.log(token)
+    if (to.meta.admin && !to.path.includes('login')) {
+        if (token) return next()
+        message.info('请登录')
+        next({ path: '/login' })
+    } else {
+        next()
+    }
 })
 
 const app = createApp(App).use(store)
